bf8c6fe888c56593885059d250b1dc75c6396ac0
pyTimK <timkristianllanto.tk@gmail.com>
2024-11-02 21:49:54 +0800

    .

--- app/custom/MainPage.tsx
+++ app/custom/MainPage.tsx
@@ -1,8 +1,3 @@
-import CheckCircleIcon from "@/components/svg/icon/CheckCircleIcon";
-import EditRoundedIcon from "@/components/svg/icon/EditRoundedIcon";
-import EmailIcon from "@/components/svg/icon/EmailIcon";
-import ExitIcon from "@/components/svg/icon/ExitIcon";
-import WarningIcon from "@/components/svg/icon/WarningIcon";
 import PageContainer from "@/components/templates/PageContainer";
 import Txt from "@/components/templates/Txt";
 
@@ -12,12 +7,6 @@ const MainPage: React.FC<MainPageProps> = ({}) => {
   return (
     <PageContainer>
       <Txt.title>TITLE</Txt.title>
-      <Txt.title>TITLE</Txt.title>
-      <CheckCircleIcon />
-      <EditRoundedIcon />
-      <ExitIcon />
-      <EmailIcon />
-      <WarningIcon />
     </PageContainer>
   );
 };
